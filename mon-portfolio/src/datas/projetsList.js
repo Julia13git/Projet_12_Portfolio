40cf6626acf968d32e18423f57efa98c9a14d8ef
Yuliia Hots <goculiya@gmail.com>
2024-05-11 12:44:55 +0200

    La suppression les fichiers inoutiles et la creation de data

---
+++ mon-portfolio/src/datas/projetsList.js
@@ -0,0 +1,127 @@
+export const projetsList = [
+    {
+        "id": "0",
+        "name": "Booki",
+        "description": "Créez la page d'accueil d'une agence de voyage avec HTML & CSS",
+        "image": "",
+        "github_link": "https://github.com/Julia13git/Projet3",
+        "github_page_link": "https://julia13git.github.io/Projet3/",
+        "skills": [
+            "Intégrer du contenu conformément à une maquette avec HTML et CSS",
+            "Implémenter une interface responsive avec HTML et CSS",
+            "Installer un environnement de développement front-end"
+        ]
+
+    },
+    {
+        "id": "1",
+        "name": "OhMyFood",
+        "description": "Améliorez l'interface d'un site mobile avec des animations CSS",
+        "image": "",
+        "github_link": "https://github.com/Julia13git/Projet4",
+        "github_page_link": "https://julia13git.github.io/Projet4/",
+        "skills": [
+            "Versionner son projet avec Git et Github",
+            "Intégrer une maquette en mobile-first",
+            "Mettre en œuvre des animations CSS"            
+        ]
+
+    },
+    {
+        "id": "2",
+        "name": "PrintIt",
+        "description": "Premiers pas sur le langage JavaScript",
+        "image": "",
+        "github_link": "https://github.com/Julia13git/Project_5",
+        "github_page_link": "https://julia13git.github.io/Project_5/",
+        "skills": [
+            "Programmer en utilisant les fondamentaux de JavaScript"
+        ]
+
+    },
+    {
+        "id": "3",
+        "name": "Sophie Bluel- Architecte d'interieur",
+        "description": "Créez une page web dynamique avec JavaScript",
+        "image": "",
+        "github_link": "https://github.com/Julia13git/Projet6",
+        "github_page_link": "https://julia13git.github.io/Projet6/",
+        "skills": [
+            "Gérer les événements utilisateurs avec JavaScript",
+            "Manipuler les éléments du DOM avec JavaScript",
+            "Récupérer les données utilisateurs dans le JavaScript via des formulaires"
+        ]
+
+    },
+    {
+        "id": "4",
+        "name": "Qwenta",
+        "description": "Planifiez le développement du site de votre client",
+        "image": "",
+        "github_link": "",
+        "github_page_link": "",
+        "skills": [
+            "Suivre le déroulement du projet grâce à un outil de gestion de projet",
+            "Présenter la solution technique",
+            "Rédiger les spécifications techniques d'un projet web à partir de besoins fonctionnels",
+            "Découper une fonctionnalités en tâches pour préparer le développement",
+            "Mettre en place une méthode de veille technologique"
+        ]
+
+    },
+    {
+        "id": "5",
+        "name": "Kasa",
+        "description": "Créez une application web de location immobilière avec React",
+        "image": "",
+        "github_link": "https://github.com/Julia13git/ProjetKasa",
+        "github_page_link": "",
+        "skills": [
+            "Développer une interface web avec Sass",
+            "Initialiser une application avec Create React App",
+            "Configurer la navigation entre les pages de l'application avec React Router",
+            "Développer des éléments de l'interface d'un site web grâce à des composants React",
+            "Mettre en œuvre des animations CSS"
+        ]
+
+    },
+    {
+        "id": "6",
+        "name": "Nina Carducci",
+        "description": "Optimisez le référencement d'un site de photographe",
+        "image": "",
+        "github_link": "https://github.com/Julia13git/Projet_9.git",
+        "github_page_link": "https://julia13git.github.io/Projet_9/",
+        "skills": [
+            "Optimiser la performance d'un site web"
+        ]
+
+    },
+    {
+        "id": "7",
+        "name": "724events",
+        "description": "Débuggez le site d'une agence d'événementiel",
+        "image": "",
+        "github_link": "https://github.com/Julia13git/Projet-10",
+        "github_page_link": "",
+        "skills": [
+            "Débugger un site web grâce aux Chrome DevTools",
+            "Rédiger un cahier de recette pour tester un site"
+        ]
+
+    },
+    {
+        "id": "8",
+        "name": "ArgentBank",
+        "description": "Implémentez le front-end d'une application bancaire avec React",
+        "image": "",
+        "github_link": "https://github.com/Julia13git/ArgentBank-website",
+        "github_page_link": "",
+        "skills": [
+            "Configurer des routes API pour la communication client / serveur",
+            "Implémenter la gestion des données avec Redux pour assurer le fonctionnement du front",
+            "Afficher les données du back end sur l'interface via des appels API"
+        ]
+
+    }
+]
