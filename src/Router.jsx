feecb91013975c9020433cdcda78652fac40f1f3
Yuliia Hots <goculiya@gmail.com>
2024-05-26 23:24:58 +0200

    la correction

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes, Switch } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Projects from "./pages/Projects";
@@ -7,15 +7,15 @@ import PageError from "./pages/PageError";
 
 function Router() {
   return (
-    <Switch>
+    <>
       <Routes>
-        <Route exact path="/" element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/project/:id" element={<ProjectPage />} />
         <Route path="*" element={<PageError />} />
       </Routes>
-    </Switch>
+    </>
   );
 }
 export default Router;
