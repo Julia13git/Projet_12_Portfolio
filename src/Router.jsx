a44cb620989ca2c38c2122f55dc6af4ba563b2df
Yuliia Hots <goculiya@gmail.com>
2024-05-26 23:22:17 +0200

    la configue pour github pages

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Switch } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Projects from "./pages/Projects";
@@ -7,15 +7,15 @@ import PageError from "./pages/PageError";
 
 function Router() {
   return (
-    <>
+    <Switch>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route exact path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/project/:id" element={<ProjectPage />} />
         <Route path="*" element={<PageError />} />
       </Routes>
-    </>
+    </Switch>
   );
 }
 export default Router;
--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
        <BrowserRouter>
-      <Router />
+      <Router basename={process.env.PUBLIC_URL}/>
     </BrowserRouter>
   </React.StrictMode>
 );
