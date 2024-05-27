2a167b417ece9c1b5a89b14881d8b9b66aa38b73
Yuliia Hots <goculiya@gmail.com>
2024-05-27 13:34:37 +0200

    les points

--- src/datas/privateData.js
+++ src/datas/privateData.js
@@ -5,14 +5,14 @@ export const privateData = {
     "telephone": "(+33) 06 42 24 27 96",
     "email": "goculiya@gmail.com",
     "skills": [
-        {   logo: "/images/icons/html-5.webp" , name: "HTML"},
-        {   logo: "/images/icons/css3.webp", name: "Css"},
-        {   logo: "/images/icons/sass.webp", name: "Sass"},  
-        {   logo: "/images/icons/javascript.webp", name: "JavaScript"},
-        {   logo: "/images/icons/react.png", name: "React"},
-        {   logo: "/images/icons/redux.webp", name: "Redux"},
-        {   logo: "/images/icons/api.webp", name: "API"},
-        {   logo: "/images/icons/seo.webp", name: "SEO"},
-        {   logo: "/images/icons/github.webp", name: "Github"}   
+        {   logo: "./images/icons/html-5.webp" , name: "HTML"},
+        {   logo: "./images/icons/css3.webp", name: "Css"},
+        {   logo: "./images/icons/sass.webp", name: "Sass"},  
+        {   logo: "./images/icons/javascript.webp", name: "JavaScript"},
+        {   logo: "./images/icons/react.png", name: "React"},
+        {   logo: "./images/icons/redux.webp", name: "Redux"},
+        {   logo: "./images/icons/api.webp", name: "API"},
+        {   logo: "./images/icons/seo.webp", name: "SEO"},
+        {   logo: "./images/icons/github.webp", name: "Github"}   
     ]  
 }
