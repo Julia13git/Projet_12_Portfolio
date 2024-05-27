3338002273f081ce653133065e4a343e5878ff4a
Yuliia Hots <goculiya@gmail.com>
2024-05-27 16:50:34 +0200

    browserrouter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import Router from "./Router";
 
 import './index.css';
@@ -9,8 +9,8 @@ import './index.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter basename={"/Projet_12_Portfolio"}>
+    <BrowserRouter basename={"/Projet_12_Portfolio"}>
       <Router />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
