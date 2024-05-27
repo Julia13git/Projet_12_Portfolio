84c5df92ce1f210d113e529890b72243b0bf2bb3
Yuliia Hots <goculiya@gmail.com>
2024-05-27 13:26:07 +0200

    basename

--- src/index.js
+++ src/index.js
@@ -8,8 +8,8 @@ import './index.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-       <BrowserRouter>
-      <Router basename={process.env.PUBLIC_URL}/>
+    <BrowserRouter basename={"/Projet_12_Portfolio"}>
+      <Router/>
     </BrowserRouter>
   </React.StrictMode>
 );
