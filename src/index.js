f18a34a89c8246796439be95fd5019ee34a56aac
Yuliia Hots <goculiya@gmail.com>
2024-05-27 16:33:36 +0200

    hashrouter

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import Router from "./Router";
+
 import './index.css';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename={"/Projet_12_Portfolio"}>
+    <HashRouter basename={"/Projet_12_Portfolio"}>
       <Router />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
