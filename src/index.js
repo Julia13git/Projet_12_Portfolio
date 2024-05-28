2d39acff958a3d291e02ef2f5fe5b72f98a7d2b3
Yuliia Hots <goculiya@gmail.com>
2024-05-28 09:38:23 +0200

    suppression homepage et basename

--- src/index.js
+++ src/index.js
@@ -2,14 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from "react-router-dom";
 import Router from "./Router";
-
 import './index.css';
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename={"/Projet_12_Portfolio"}>
+    <BrowserRouter >
       <Router />
     </BrowserRouter>
   </React.StrictMode>
